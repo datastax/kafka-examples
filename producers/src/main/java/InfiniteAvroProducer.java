7411b9c0984fb3902ff64e7f30318f2f9cdb4010
tomekl007 <tomasz.lelek@datastax.com>
2018-11-15 13:19:09 +0100

    simple pre-populating producer not-need to be multi-threaded. Only the infinite one

--- producers/src/main/java/InfiniteAvroProducer.java
+++ producers/src/main/java/InfiniteAvroProducer.java
@@ -1,3 +1,4 @@
+import com.google.common.util.concurrent.RateLimiter;
 import io.confluent.kafka.serializers.KafkaAvroSerializer;
 import io.confluent.kafka.serializers.KafkaAvroSerializerConfig;
 import org.apache.avro.Schema;
@@ -13,28 +14,34 @@ import org.slf4j.LoggerFactory;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.time.Duration;
 import java.time.Instant;
 import java.util.Properties;
-import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicLong;
+import java.util.stream.IntStream;
 
-public class AvroProducer {
+
+public class InfiniteAvroProducer {
 
   private static final String TOPIC = "avro-stream";
   private static final int SEGMENTS_PER_RECORD = 10;
   private static final int FIELDS_PER_SEGMENT = 10;
-  private static final int TOTAL_RECORDS = 100_000;
+
+  private static final int MAX_REQUEST_PER_SECOND = 30_000;
+  private static final RateLimiter RATE_LIMITER = RateLimiter.create(MAX_REQUEST_PER_SECOND);
+  private static final AtomicLong ID_GENERATOR = new AtomicLong();
 
   // Report number of records sent every this many seconds.
   private static final long PROGRESS_REPORTING_INTERVAL = 5;
 
-  private static Logger log = LoggerFactory.getLogger("BigAvroProducer");
+  private static Logger log = LoggerFactory.getLogger("InfiniteBigAvroProducer");
+  private static final int NUMBER_OF_THREADS = Runtime.getRuntime().availableProcessors();
+  private static final ExecutorService executorService = Executors.newFixedThreadPool(NUMBER_OF_THREADS);
 
-  public static void main(String[] args) throws InterruptedException {
+  public static void main(String[] args) {
 
     Properties props = new Properties();
     props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
@@ -69,7 +76,6 @@ public class AvroProducer {
     // will be signalled every time a "send" completes. This latch allows us
     // to wait for all sends to complete before terminating the program.
     AtomicLong errorCount = new AtomicLong();
-    CountDownLatch requestLatch = new CountDownLatch(TOTAL_RECORDS);
 
 
     // Create a counter to track the number of records we've successfully
@@ -85,7 +91,6 @@ public class AvroProducer {
       } else {
         successCount.incrementAndGet();
       }
-      requestLatch.countDown();
     };
 
     ScheduledExecutorService scheduler = Executors.newScheduledThreadPool(1);
@@ -94,27 +99,23 @@ public class AvroProducer {
         2, PROGRESS_REPORTING_INTERVAL, TimeUnit.SECONDS);
 
 
-    for (int k = 0; k < TOTAL_RECORDS; k++) {
-      GenericRecord thisKeyRecord = new GenericData.Record(avroKeySchema);
-      GenericRecord thisValueRecord = new GenericData.Record(avroValueSchema);
-      for (int j = 0; j < SEGMENTS_PER_RECORD; j++) {
-        GenericRecord nestedRecord = new GenericData.Record(avroValueSchema.getField("segment" + j).schema().getTypes().get(1));
-        for (int i = 0; i < FIELDS_PER_SEGMENT; i++) {
-          nestedRecord.put("segment" + j + "_" + i, Integer.toString(i));
+    IntStream.range(0, NUMBER_OF_THREADS).forEach(ignore ->
+    executorService.submit(() -> {
+      while(true){
+        GenericRecord thisKeyRecord = new GenericData.Record(avroKeySchema);
+        GenericRecord thisValueRecord = new GenericData.Record(avroValueSchema);
+        for (int j = 0; j < SEGMENTS_PER_RECORD; j++) {
+          GenericRecord nestedRecord = new GenericData.Record(avroValueSchema.getField("segment" + j).schema().getTypes().get(1));
+          for (int i = 0; i < FIELDS_PER_SEGMENT; i++) {
+            nestedRecord.put("segment" + j + "_" + i, Integer.toString(i));
+          }
+          thisValueRecord.put("segment" + j, nestedRecord);
         }
-        thisValueRecord.put("segment" + j, nestedRecord);
+        thisKeyRecord.put("key", Long.toString(ID_GENERATOR.incrementAndGet()));
+
+        RATE_LIMITER.acquire();
+        producer.send(new ProducerRecord<>(TOPIC, thisKeyRecord, thisValueRecord), postSender);
       }
-      thisKeyRecord.put("key", Integer.toString(k));
-      producer.send(new ProducerRecord<>(TOPIC, thisKeyRecord, thisValueRecord), postSender);
-    }
-
-    // Wait for sends to complete.
-    requestLatch.await();
-
-    // Stop the thread that periodically reports progress.
-    scheduler.shutdown();
-    long duration = Duration.between(start, Instant.now()).getSeconds();
-    log.info("Completed loading {}/{} records to Kafka in {} seconds",
-        TOTAL_RECORDS - errorCount.get(), TOTAL_RECORDS, duration);
+    }));
   }
 }
